d68c57dd630cfba50a4821258359df1cccc260ea
skyholeBishop <51252247+bigLRRH@users.noreply.github.com>
2025-03-25 15:20:50 +0800

    无敌了！

--- src/trie.rs
+++ src/trie.rs
@@ -15,32 +15,17 @@ impl Trie {
         let mut node = &mut self.root;
         for c in word.chars() {
             node = node.children.entry(c).or_insert(TrieNode::new());
-            node.count += 1;
+            node.weight += 1;
         }
     }
 
     // todo 修bug
-    pub fn generalized_longest_common_prefix(&self, min_count: usize) -> String {
+    pub fn generalized_longest_common_prefix(&self, min_weight: usize) -> String {
         let mut common_prefix = String::new();
         let mut node = &self.root;
 
-        loop {
-            let candidates: Vec<_> = node
-                .children
-                .iter()
-                .filter(|(_, child)| child.count >= min_count)
-                .collect();
-
-            if candidates.is_empty() {
-                break;
-            }
-
-            let (c, next_node) = candidates
-                .iter()
-                .max_by_key(|(_, child)| child.count)
-                .unwrap();
-
-            common_prefix.push(**c);
+        while let Some((c, next_node)) = node.get_max_count_child(min_weight) {
+            common_prefix.push(*c);
             node = next_node;
         }
 
@@ -51,7 +36,7 @@ impl Trie {
 #[derive(Default)]
 struct TrieNode {
     children: HashMap<char, TrieNode>,
-    count: usize,
+    weight: usize, // 记录当前节点的个数
 }
 
 impl TrieNode {
@@ -59,7 +44,24 @@ impl TrieNode {
         Default::default()
     }
 
-    fn is_end(&self) -> bool {
-        self.children.is_empty()
+    /// 获取权重最大的子节点，如果有多个，继续比较子节点的子节点
+    fn get_max_count_child(&self, min_weight: usize) -> Option<(&char, &TrieNode)> {
+        self.children
+            .iter()
+            .filter(|(_, node)| node.weight >= min_weight)
+            .max_by(|a, b| {
+                a.1.weight.cmp(&b.1.weight).then_with(|| {
+                    match (
+                        a.1.get_max_count_child(min_weight),
+                        b.1.get_max_count_child(min_weight),
+                    ) {
+                        (Some((_, a_node)), Some((_, b_node))) => a_node.weight.cmp(&b_node.weight),
+                        (Some(_), None) => std::cmp::Ordering::Greater,
+                        (None, Some(_)) => std::cmp::Ordering::Less,
+                        // * 实际上几乎不会出现这种情况
+                        (None, None) => std::cmp::Ordering::Equal,
+                    }
+                })
+            })
     }
 }
